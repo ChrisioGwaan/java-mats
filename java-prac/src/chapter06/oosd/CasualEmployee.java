8b81696601bf6a9afcec1a1157a45fd80b9a501e
ChrisioGwaan <chris322322@gmail.com>
2023-08-07 11:27:00 +0800

    [NEW] inheritance examples

---
+++ java-prac/src/chapter06/oosd/CasualEmployee.java
@@ -0,0 +1,40 @@
+package chapter06.oosd;
+
+class CasualEmployee extends Employee {
+    private double hourlySalary;
+    private double overtimeRate;
+    final private int OVERTIME_START = 36; // Overtime paid starting from 36 hours and the value cannot be changed
+
+    public CasualEmployee(int ID, String name, String employeeType, double hourlyworked, double hourlySalary, double overtimeRate) {
+        super(ID, name, employeeType, hourlyworked);
+        this.hourlySalary = hourlySalary;
+        this.overtimeRate = overtimeRate;
+    }
+
+    public double getHourlySalary() {
+        return this.hourlySalary;
+    }
+
+    public void setHourlySalary(double hourlySalary) {
+        this.hourlySalary = hourlySalary;
+    }
+
+    public double getOvertimeRate() {
+        return this.overtimeRate;
+    }
+
+    public void setOvertimeRate(double overtimeRate) {
+        this.overtimeRate = overtimeRate;
+    }
+
+    @Override
+    public double calculateSalary() {
+        // TODO: Implement an algorithm to calculate the salary for casual employee
+        // The algorithm is to use hourlySalary * hourlyworked to calculate the salary
+        // if the hourlyworked is less than or equal to 36 hours
+        // return the calculated salary = hourlySalary * hourlyworked
+        // if the hourlyworked is more than 36 hours
+        // return the calculated salary = hourlySalary * OVERTIME_START + (hourlyworked - OVERTIME_START) * overtimeRate
+        return 0;
+    }
+}
---
+++ java-prac/src/chapter06/oosd/SalariedEmployee.java
@@ -0,0 +1,38 @@
+package chapter06.oosd;
+
+// @Entity
+class SalariedEmployee extends Employee {
+    private double monthlySalary;
+
+    // Paid leave hours for salaried employee
+    private double paidLeaveHours;
+
+    public SalariedEmployee(int ID, String name, String employeeType, double hourlyworked, double monthlySalary) {
+        super(ID, name, employeeType, hourlyworked);
+        this.monthlySalary = monthlySalary;
+    }
+
+    public double getMonthlySalary() {
+        return this.monthlySalary;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    public double getPaidLeaveHours() {
+        return this.paidLeaveHours;
+    }
+
+    public void setPaidLeaveHours(double paidLeaveHours) {
+        this.paidLeaveHours = paidLeaveHours;
+    }
+
+    @Override
+    public double calculateSalary() {
+        // TODO: Implement an algorithm to calculate the salary for salaried employee
+        // Direct access the monthlySalary field
+        // Return the calculated salary according to the algorithm
+        return 0;
+    }
+}
