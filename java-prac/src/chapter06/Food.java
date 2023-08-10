d59a8461672fef01472045a547020e542b1d5db0
evaa-e <evanthianything@gmail.com>
2023-08-10 20:36:41 +1000

    deafultconstuctor?

--- java-prac/src/chapter06/Food.java
+++ java-prac/src/chapter06/Food.java
@@ -5,21 +5,29 @@ class Food {
 
     // TODO: Create a private String variable called name
     private String name;
-}
+
 
     // TODO: Create a private double variable called calories
     private double calories;
-}
+
 
     // TODO: Create a private int variable called quantity
     private int quantity;
-}
+
 
     // TODO: Create a private boolean variable called isHealthy
     private boolean isHealthy;
 }
 
     // TODO: Default Constructor, initialise all variables to "", 0, or false
+    Food() {
+        name = "";
+        isHealthy = false;
+        calories = 0;
+        quantity = 0; 
+
+    }
+
 
     // TODO: Another Constructor that takes in a String name, double calories, int quantity, and boolean isHealthy
 
