5fe2f5e21dc43dac480d3ae30aedee6930e07c97
ChrisioGwaan <chris322322@gmail.com>
2023-08-10 15:04:08 +0800

    [UPDATE] chapt 3 prac2 answers

--- java-prac/src/chapter03/prac2.java
+++ java-prac/src/chapter03/prac2.java
@@ -4,35 +4,6 @@ import java.util.Scanner;
 
 class prac2 {
     public static void main(String[] args) {
-    // Write your code anywhere you need
-      Scanner scnr = new Scanner(System.in);
-
-    int a;
-    int b;
-    int c;
-    int n;
-
-    a = scnr.nextInt();
-    b = scnr.nextInt();
-    c = scnr.nextInt();
-    n = scnr.nextInt();
-
-    if (n <= 2) {
-        System.out.println("No, that doesn’t work.");
-    }
-    else {
-        if (Math.pow(a, n) + Math.pow(b, n) == Math.pow(c, n)) {
-
-            System.out.println("Holy smokes, fermat was wrong!");
-        }
-        else {
-
-            System.out.println("Fermat was correct!");
-
-            }
-            scnr.close();
-    
-        }
         Scanner scnr = new Scanner(System.in);
 
         int a, b, c, n;
